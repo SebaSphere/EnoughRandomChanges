359ab7e0aeeda233e2cb2f763366e274e449ab91
SebaSphere <SebaSphereEmails@gmail.com>
2021-05-12 15:14:09 -0400

    Initial commit

---
+++ src/main/java/dev/sebastianb/enoughrandomchanges/EnoughRandomChanges.java
@@ -0,0 +1,16 @@
+package dev.sebastianb.enoughrandomchanges;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class EnoughRandomChanges implements ModInitializer {
+
+    public static String MOD_ID = "enoughrandomchanges";
+    public static Logger LOGGER = Logger.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
