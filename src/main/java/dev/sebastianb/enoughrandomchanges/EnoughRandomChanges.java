0fb62345a91ea9d0ccf28c2e92d371901cf6b077
cavemeat <c4v3m3t@gmail.com>
2021-05-12 15:45:55 -0400

    aw yeah woo yeah again

--- src/main/java/dev/sebastianb/enoughrandomchanges/EnoughRandomChanges.java
+++ src/main/java/dev/sebastianb/enoughrandomchanges/EnoughRandomChanges.java
@@ -8,7 +8,7 @@ public class EnoughRandomChanges implements ModInitializer {
 
     public static String MOD_ID = "enoughrandomchanges";
     public static Logger LOGGER = Logger.getLogger(MOD_ID);
-    //pfpfppfpfpfpfppfpfppfpfpfppfpfpfppfpfpffppfpffppfpffp
+    //pfpfppfpfpfpfppfpfppfpfpfppfpfpfppfpfpffppfpffppfpff
     @Override
     public void onInitialize() {
         
